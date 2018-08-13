919bb445ced6253b931634a17e22e69e81ca4c81
asutosh <asutosh407@gmail.com>
2018-08-13 20:16:27 +0530

    generate address using admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Clients
+from .models import Client
 
 
-admin.site.register(Clients)
+admin.site.register(Client)
--- clients/models.py
+++ clients/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.db.models.signals import pre_save
-
+import uuid
 # Create your models here.
-class Clients(models.Model):
+class Client(models.Model):
     name=models.CharField(max_length=120,null=False,blank=False)
-    token=models.CharField(max_length=120,null=False,blank=False)
+    token=models.UUIDField(default=uuid.uuid4,editable=False,null=False,blank=False)
     
     def __str__(self):
         return self.name
         
-   
+    class ReadonlyMeta:
+        readonly = ["token"]
