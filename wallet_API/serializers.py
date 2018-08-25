f0dcdc2e83542a69e61ed2c3575c8e9f5df81aa8
asutosh <asutosh407@gmail.com>
2018-08-25 11:16:54 +0530

    api

--- wallet_API/serializers.py
+++ wallet_API/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Wallet
 
-class WalletSerializer(serializers.HyperlinkedSerializers):
+class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model= Wallet
         fields=(
